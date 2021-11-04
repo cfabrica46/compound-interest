3c339fa56d21de35c283fcacf296602feeea66a9
Cesar <c.caycho46@gmail.com>
2021-11-03 22:24:08 -0500

    completed

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,10 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Balance = styled.span`
-  font-size: 36px;
-  margin-top: 12px;
-  display: inline-block;
-  color: black;
-`
+    font-size: 36px;
+    margin-top: 12px;
+    display: inline-block;
+    color: black;
+`;
 
-export default Balance
+export default Balance;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,13 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Button = styled.button`
-  background: palevioletred;
-  color: white;
-  font-size: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
-  border-radius: 3px;
-  cursor: pointer;
-`
+    background: palevioletred;
+    color: white;
+    font-size: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid palevioletred;
+    border-radius: 3px;
+    cursor: pointer;
+`;
 
-export default Button
+export default Button;
